468280f0a010d9ea46e3e77cd8f82a4f049d27de
Alex Sandro <geniofuturo@gmail.com>
2013-03-27 17:17:27 -0300

    removendo código de configuração antiga.

--- script.php
+++ script.php
@@ -28,24 +28,6 @@ class com_pbeventsInstallerScript
 	 */
 	function install($parent)
 	{
-		// load the language strings
-		$translator = JFactory::getLanguage();
-		$translator->load('com_pbevents', JPATH_ADMINISTRATOR);
-		
-		// $parent is the class calling this method
-		$db =& JFactory::getDbo();
-		
-		$config = new JObject(array(
-			'id' => 1,
-			'email_failed_subject' => $translator->_("COM_PBEVENTS_EMAIL_FAILED_SUBJECT"),
-			'email_failed_body' => $translator->_("COM_PBEVENTS_EMAIL_FAILED_BODY"),
-			'email_success_subject' =>  $translator->_("COM_PBEVENTS_EMAIL_SUCCESSFUL_SUBJECT"),
-			'email_success_body'=> $translator->_("COM_PBEVENTS_EMAIL_SUCCESSFUL_BODY"),
-			'send_notifications_to' => $translator->_("COM_PBEVENTS_EMAIL_TIP_SAMPLE"),
-			'date_picker_locale' => $translator->_("COM_PBEVENTS_LOCALE")
-		));
-		
-		$db->insertObject('#__pbevents_config', $config, 'id');
 	}
 }
 
