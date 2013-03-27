c34a434faf731d636161aeb6bce508ca8e3c5938
Alex Sandro <geniofuturo@gmail.com>
2013-03-27 01:57:37 -0300

    Adicionando 'fields text, textarea' com correção dos valores 'default'.

---
+++ admin/fields/conftextarea.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+JFormHelper::loadFieldClass('textarea');
+
+/**
+ * Form Field class for the Joomla Platform.
+ * Provides and input field for e-mail addresses
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ * @link        http://www.w3.org/TR/html-markup/input.email.html#input.email
+ * @see         JFormRuleEmail
+ * @since       11.1
+ */
+class JFormFieldConftextarea extends JFormFieldTextarea
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  11.1
+	 */
+	protected $type = 'conftextarea';
+
+	/**
+	 * Method to get the field input markup for e-mail addresses.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getInput()
+	{
+		// forçando um valor de configuração padrão.
+		if (!$this->value && $this->element["default"])
+		{
+			$this->value = JText::_($this->element["default"]);
+		}
+		// renderizando o input.
+		return parent::getInput();
+	}
+}
+?>
