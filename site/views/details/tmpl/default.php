04850af3393f20f606473418360163d77a5dc333
Alex Sandro <geniofuturo@gmail.com>
2013-03-27 22:21:10 -0300

    usando o mesmo template como base para apresentação de conteúdo.

--- site/views/details/tmpl/default.php
+++ site/views/details/tmpl/default.php
@@ -2,15 +2,10 @@
 /**
 * @license	GNU General Public License version 2 or later; see LICENSE.txt
 */
- 
 // no direct access
- 
-defined( '_JEXEC' ) or die( 'Restricted access' );
-jimport( 'joomla.application.component.view');
+defined('_JEXEC') or die('Restricted access');
 
 $doc = JFactory::getDocument();
-$config = JFactory::getConfig();
-
 $doc->addStyleSheet(JUri::root(false).'components/com_pbevents/css/com_pbevents.default.css');
 
 $inscription = new Inscription($this->event);
@@ -26,26 +21,27 @@ $inscription = new Inscription($this->event);
 	<h3><?php echo JText::_("COM_PBEVENTS_DATES") ?></h3>
 	
 	<?php foreach($this->event->dates as $date): ?>
-		<p><?php echo PBEventsHelper::formatDateHours($date->date, 
-					$date->hstart, $date->hend, $date->henable); ?>
+		<p><?php echo PBEventsHelper::formatDateHours($date->date, $date->hstart, $date->hend, $date->henable); ?>
 			<i><?php echo $date->description ?></i>
 		</p>
 	<?php endforeach ?>
 </div>
 
 <div class="event-info">
-	<p>
-		<?php echo sprintf(JText::sprintf("COM_PBEVENTS_GLOBAL_HOUR", $this->event->hstart, $this->event->hend)) ?>
-	</p>
-</div>
-			
-<div class="event-info">
-	<p><?php echo JText::_("COM_PBEVENTS_PANELIST") ?><i><?php echo $this->event->panelist ?></i></p>
+	<p><?php echo sprintf(JText::sprintf("COM_PBEVENTS_GLOBAL_HOUR", $this->event->hstart, $this->event->hend)) ?></p>
 </div>
 
-<div class="event-info">
-	<p><?php echo JText::_("COM_PBEVENTS_PRICE") ?><i><?php echo PBEventsHelper::formatMoney($this->event->price) ?></i></p>
-</div>
+<?php if ($this->event->panelist): ?>
+	<div class="event-info">
+		<p><?php echo JText::_("COM_PBEVENTS_PANELIST") ?><i><?php echo $this->event->panelist ?></i></p>
+	</div>
+<?php endif ?>
+
+<?php if ($this->event->price && $this->event->price != 0): ?>
+	<div class="event-info">
+		<p><?php echo JText::_("COM_PBEVENTS_PRICE") ?><i><?php echo PBEventsHelper::formatMoney($this->event->price) ?></i></p>
+	</div>
+<?php endif ?>
 
 <?php if ($this->event->show_counter == 1): ?>
 	<div id="event-people-number">
--- site/views/pbevents/tmpl/default.php
+++ site/views/pbevents/tmpl/default.php
@@ -16,73 +16,40 @@ $doc->addStyleSheet(JUri::root(false).'components/com_pbevents/css/com_pbevents.
 
 $option = JRequest::getCmd('option');
 $view = JRequest::getCmd('view');
-?>
 
+// template de detalhes compõe o corpo das lista de eventos.
+$sharedTemplatePath = implode(DIRECTORY_SEPARATOR, array(JPATH_COMPONENT_SITE,"views","details","tmpl","default.php"));
+?>
 <?php if (isset($this->events) && count($this->events) > 0): ?>
-	
-	<?php foreach ($this->events as $event): ?>
-		<form action="<?php echo JRoute::_('index.php?option=com_pbevents') ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
-			<h2><?php echo $event->title ?></h2>
-			<div class="event-info">
-				<p><?php echo $event->description ?></p>
-			</div>
-			
-			<?php $inscription = new Inscription($event) ?>
-			
-			<div class="event-info event-dates">
-				<h3><?php echo JText::_("COM_PBEVENTS_DATES") ?></h3>
+	<?php foreach ($this->events as $this->event): ?>
+		<div id="event-info-group">
+			<form action="<?php echo JRoute::_('index.php?option=com_pbevents') ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
 				
-				<?php foreach($event->dates as $date): ?>
-					<p><?php echo PBEventsHelper::formatDateHours($date->date, 
-								$date->hstart, $date->hend, $date->henable); ?>
-						<i><?php echo $date->description ?></i>
-					</p>
-				<?php endforeach ?>
-			</div>
-			
-			<div class="event-info">
-				<p>
-					<?php echo sprintf(JText::sprintf("COM_PBEVENTS_GLOBAL_HOUR", $event->hstart, $event->hend)) ?>
-				</p>
-			</div>
-						
-			<div class="event-info">
-				<p><?php echo JText::_("COM_PBEVENTS_PANELIST") ?><i><?php echo $event->panelist ?></i></p>
-			</div>
-			
-			<div class="event-info">
-				<p><?php echo JText::_("COM_PBEVENTS_PRICE") ?><i><?php echo PBEventsHelper::formatMoney($event->price) ?></i></p>
-			</div>
-			
-			<?php if ($event->show_counter == 1): ?>
-				<div id="event-people-number">
-					<p><?php echo sprintf(JText::_("COM_PBEVENTS_PARTICIPATE_NUMBER"), $inscription->current()) ?></p>
-					<p><?php echo sprintf(JText::_("COM_PBEVENTS_PARTICIPATING_NUMBER"), $inscription->count()) ?></p>
-				</div>
-			<?php endif ?>
-			
-			<div class="div-input-submit">
-				<?php if(!$inscription->isQuestUser()): ?>
-					<?php if(!$inscription->checkUser()): ?>
-						<?php if (!$inscription->isClosed()): ?>
-							<input type="submit" class="submit" name="event-<?php echo $event->id ?>" value="<?php echo JText::_("COM_PBEVENTS_SUBMIT_REGISTER") ?>" />
-							<input type="hidden"  name="event" value="<?php echo $event->id ?>" />
+				<?php include $sharedTemplatePath; // incluindo o template de detalhes. usando a mesma base ?>
+				
+				<div class="div-input-submit">
+					<?php if(!$inscription->isQuestUser()): ?>
+						<?php if(!$inscription->checkUser()): ?>
+							<?php if (!$inscription->isClosed()): ?>
+								<input type="submit" class="submit" name="event-<?php echo $this->event->id ?>" value="<?php echo JText::_("COM_PBEVENTS_SUBMIT_REGISTER") ?>" />
+								<input type="hidden"  name="event" value="<?php echo $this->event->id ?>" />
+							<?php else: ?>
+								<div><p id="alert-max-people"><?php echo JText::_("COM_PBEVENTS_MAX_PEOPLE_ACHIEVED") ?></p></div>
+							<?php endif ?>
 						<?php else: ?>
-							<div><p id="alert-max-people"><?php echo JText::_("COM_PBEVENTS_MAX_PEOPLE_ACHIEVED") ?></p></div>
+							<div><p id="alert-user-registred"><?php echo JText::_("COM_PBEVENTS_REGISTRED_USER_ALERT") ?></p></div>
 						<?php endif ?>
 					<?php else: ?>
-						<div><p id="alert-user-registred"><?php echo JText::_("COM_PBEVENTS_REGISTRED_USER_ALERT") ?></p></div>
+						<input type="submit" class="event-submit" name="event-<?php echo $this->event->id ?>" value="<?php echo JText::_("COM_PBEVENTS_SUBMIT_REGISTER") ?>" />
+						<input type="hidden"  name="event" value="<?php echo $this->event->id ?>" />
 					<?php endif ?>
-				<?php else: ?>
-					<input type="submit" class="event-submit" name="event-<?php echo $event->id ?>" value="<?php echo JText::_("COM_PBEVENTS_SUBMIT_REGISTER") ?>" />
-					<input type="hidden"  name="event" value="<?php echo $event->id ?>" />
-				<?php endif ?>
-			</div>
-			<div>
-				<input type="hidden" name="task" value="" />
-				<?php echo JHtml::_('form.token') ?>
-			</div>
-		</form>
+				</div>
+				<div>
+					<input type="hidden" name="task" value="" />
+					<?php echo JHtml::_('form.token') ?>
+				</div>
+			</form>
+		</div>
 	<?php endforeach?>
 <?php else: ?>
 	<div><p id="alert-no-article"><?php echo JText::_("COM_PBEVENTS_NO_EVENTS") ?></p></div>
