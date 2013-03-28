f1eacfa49cc2515ca3ee73103540c1e8255667b3
Alex Sandro <geniofuturo@gmail.com>
2013-03-28 19:19:04 -0300

    criando a lógica padrão de view.

---
+++ admin/views/events/view.html.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     ...
+ * @subpackage  com_pbevents
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * View class for a list of articles.
+ *
+ * @package     ...
+ * @subpackage  com_pbevents
+ * @since       ...
+ */
+class PBEventsViewEvents extends JViewLegacy
+{
+	public function display($tpl) {
+		
+		parent::display($tpl);
+	}
+	
+}
