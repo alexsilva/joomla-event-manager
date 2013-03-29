d4f958a772bc12626d2ff87bf4eb169e3a8cb394
Alex Sandro <geniofuturo@gmail.com>
2013-03-29 02:32:58 -0300

    adicionando uma tool bar a 'view'.

--- admin/views/listattendees/view.html.php
+++ admin/views/listattendees/view.html.php
@@ -21,11 +21,32 @@ class PBEventsViewListAttendees extends JViewLegacy
 	public function display($tpl = null) {
 		$input =& JFactory::getApplication()->input;
 		
+		$params =& JComponentHelper::getParams("com_pbevents");
+		$limit_list = $params->get("alist_limit", 6);
+		
+		$input->set("limit", $input->get("limit", $limit_list));
+		
 		$this->pagination = $this->get("Pagination");
 		$this->item = $this->get("Items");
 		
+		// id da seção
 		$this->event_id = $input->get("id",0,"integer");
+		$this->addToolbar();
 		
 		parent::display($tpl);
 	}
+	
+	private function addToolbar() {
+		JToolBarHelper::title(JText::_('COM_PBEVENTS_EVENTS_MANAGER').' '.JText::_('COM_PBEVENTS_ATTENDEES'),'generic.png');
+		
+		$canDo = PBEventsHelper::getActions();
+		
+		// definindo níves de permissões.
+		if ($canDo->get('core.delete'))
+		{
+			JToolBarHelper::deleteList('','deleteattendee');
+		}
+	}
 }
+
+
