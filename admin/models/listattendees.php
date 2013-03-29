aef3dfebf267bdd686265fb65365c69b23dfc39f
Alex Sandro <geniofuturo@gmail.com>
2013-03-29 01:58:38 -0300

    adicionando modelo padrão para lista de participantes.

---
+++ admin/models/listattendees.php
@@ -0,0 +1,16 @@
+<?php
+
+class PBEventsModelListEvents extends JModelList
+{	
+	/**
+	 * Method to get a JDatabaseQuery object for retrieving the data set from a database.
+	 *
+	 * @return  JDatabaseQuery   A JDatabaseQuery object to retrieve the data set.
+	 *
+	 * @since   12.2
+	 */
+	protected function getListQuery() {
+		
+	}
+	
+}
---
+++ admin/views/listattendees/view.html.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     ...
+ * @subpackage  com_pbevents
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * View class for a list of articles.
+ *
+ * @package     ...
+ * @subpackage  com_pbevents
+ * @since       ...
+ */
+class PBEventsViewListAttendees extends JViewLegacy 
+{
+	public function display($tpl = null) {
+		
+		parent::display($tpl);
+	}
+}
